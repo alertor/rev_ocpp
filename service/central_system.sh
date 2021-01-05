1638a718b25aea487633319b7c0beeb08a508e3a
Travis <travis@stealthtechnologies.com.au>
2021-01-05 16:48:33 +0800

    Fix script venv location, env file

--- service/central_system.sh
+++ service/central_system.sh
@@ -4,4 +4,4 @@ export ROOTPATH=/home/ubuntu/rev_ocpp
 export PYTHONPATH=$ROOTPATH/src
 
 cd $ROOTPATH || exit
-./venv/bin/python venv/bin/gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker
+./.venv/bin/python .venv/bin/gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker
