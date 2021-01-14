f025b03ec4cab8043a51b1fe7b5a21ea0b3181da
travip <travis@stealthtechnologies.com.au>
2021-01-14 09:10:24 +0800

    Increase number of workers for gunicorn proces

--- service/central_system.sh
+++ service/central_system.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO: Dynamically generate path when installed
 export ROOTPATH=/home/ubuntu/rev_ocpp
 export PYTHONPATH=$ROOTPATH/src
 
 cd $ROOTPATH || exit
-./.venv/bin/python .venv/bin/gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker
+./.venv/bin/python .venv/bin/gunicorn main:app -w 8 -k uvicorn.workers.UvicornWorker
